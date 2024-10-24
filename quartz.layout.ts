d8e9702c4e47664cbd82da6434525e3139ada151
Mateusz <mateusz.witomski@hotmail.com>
2024-10-24 23:04:08 +0200

    Orion Lab | Rebuild solution | #1
    * Removed folder docs,
    * Modified styles for layout of the site,
    * Added custom toolbars,
    * Removed arrow icons from Explorer section

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -16,19 +16,14 @@ export const sharedPageComponents: SharedLayout = {
 
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
+  header: [Component.DesktopOnly(Component.Spacer()), Component.Search(), Component.Darkmode()],
   beforeBody: [
     Component.Breadcrumbs(),
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
   ],
-  left: [
-    Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
-    Component.DesktopOnly(Component.Explorer()),
-  ],
+  left: [Component.PageTitle(), Component.DesktopOnly(Component.Explorer())],
   right: [
     Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
@@ -39,12 +34,11 @@ export const defaultContentPageLayout: PageLayout = {
 
 // components for pages that display lists of pages  (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
+  header: [Component.DesktopOnly(Component.Spacer()), Component.Search(), Component.Darkmode()],
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Search(),
-    Component.Darkmode(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [],
